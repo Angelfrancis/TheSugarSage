7a92638c895776a0f5d49d87ae37b328723eaefa
Angel Francis <50880391+DevelopedByAngel@users.noreply.github.com>
2020-06-07 17:38:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 $('#loader > img').css('width',$(window).width()*95/100+'px');
-if($(window).width()<1000){
+if($(window).width()<1000)
 		$('#loader > img').css('width',$(window).width()*95/100+'px');
+else
+		$('#loader > img').css('width',$(window).width()*0/100+'px');
+var homed="";
+var aboutd="";
+var homev=0;
 
-}
-		else{
-			$('#loader > img').css('width',$(window).width()*0/100+'px');
 
-		}
+$(window).on("load",function()
+	{setTimeout(function(){
+			$('#loader').css('opacity','0%');
+			$('#loader').css('z-index','-1');
+		},5000);	});
+
+
 
 function play(x)
 {
@@ -17,6 +25,8 @@ else{
 	x.pause();
 $('#play').attr('src','play.png');}
 };
+
+
 function next(){
 var c=$('#audio').attr('class');
 if(c=='8')
@@ -28,6 +38,8 @@ $('#desc > img').attr('src',parseInt(c)+1+'.jpg');
 var x=$('#audio')[0];
 x.play();
 };
+
+
 function previous(){
 var c=$('#audio').attr('class');
 if(c=='1')
@@ -38,8 +50,9 @@ $('#play').attr('src','pause.png');
 $('#desc > img').attr('src',parseInt(c)-1+'.jpg');
 var x=$('#audio')[0];
 x.play();
-
 };
+ 
+
 function playing(x)
 {
 $('#lp').css('width',(x.currentTime/x.duration)*100+'%');
@@ -49,13 +62,25 @@ function forward()
 	var x=$('#audio')[0];//$('') is always a array also for id so [] is used to access the element
 	x.currentTime=x.currentTime+5;
 };
+
+
 function backward()
 {
 	var x=$('#audio')[0];//$('') is always a array also for id so [] is used to access the element
 	x.currentTime=x.currentTime-5;
-}
+};
+
 $(document).ready(function()
 	{
+		if($(window).width()<1000){
+				$('#playing').css('bottom','18%');
+				$('#desc > img').css('width',$(window).width()*60/100+"px");
+		}
+		else{
+				$('#playing').css('bottom','25%');	
+				$('#desc > img').css('height',$(window).height()*40/100+"px");
+				$('#desc > img').css('width',$(window).width()*10/100+"px");
+		}
 		var top=50;
 		for(i=1;i<=8;i++)
 		{
@@ -63,6 +88,9 @@ $(document).ready(function()
 			top=top+250;
 	}
 	});
+
+
+
 function change(x)
 {
 	var i=parseInt(x.id);
@@ -73,6 +101,8 @@ $('#desc > img').attr('src',i+'.jpg');
 var x=$('#audio')[0];
 x.play();
 };
+
+
 function mouseover(x)
 	{
 	var i=parseInt(x.id);
@@ -80,6 +110,8 @@ function mouseover(x)
 	$('#'+i+'> p').css('opacity','100%');
 
 	};
+
+
 function mouseout(x)
 	{
 var i=parseInt(x.id);
@@ -91,44 +123,43 @@ var i=parseInt(x.id);
 		$('#underline').css('left','18px');
 		$('#underline').css('width','80px');
 	});
+
+
+
 $('#about').mousemove(function()
 {
 	$('#underline').css('left','120px');
 	$('#underline').css('width','80px');
 });
+
+
 $('#writeto').mousemove(function()
 {
 	$('#underline').css('left','222px');
 	$('#underline').css('width','110px');
 });
-$(document).ready(function()
+
+
+$('#menu').mouseout(function()
 	{
-		if($(window).width()<1000){
-		$('#playing').css('bottom','18%');
-
-		$('#desc > img').css('width',$(window).width()*60/100+"px");
-}
+		if(homev==0){
+		$('#underline').css('left','18px');
+		$('#underline').css('width','80px');}
 		else{
-		$('#playing').css('bottom','25%');	
+			$('#underline').css('left','120px');
+	$('#underline').css('width','80px');}
+	});
 
-				$('#desc > img').css('height',$(window).height()*40/100+"px");
-				$('#desc > img').css('width',$(window).width()*10/100+"px");
-		}});
 $(window).ready(function()
 	{ 
 		$('#loader').css('width',$(window).width()+"px");
 		$('#loader').css('height',$(window).height()+"px");
-		
-			setTimeout(function(){
-		$('#loader').remove();		},5300);
-
+		homed="<div id='homediv'>"+$('#homediv').html()+"</div>";
+		aboutd="<div id='aboutdiv'>"+$('#aboutdiv').html();+"</div>";
+		 homev=0
 	});
 
-$(window).on("load",function()
-	{setTimeout(function(){
-			$('#loader').css('opacity','0%');
-			$('#loader').css('z-index','-1');
-		},5000);	});
+
 $(window).resize(function()
 	{
 		if($(window).width()<1000)
@@ -136,8 +167,34 @@ $(window).resize(function()
 		else
 		$('#playing').css('bottom','25%');	
 });
+
+
 $(window).load(function()
 {
 $('#loader').css('width',$(window).width()+"px");
 		$('#loader').css('height',$(window).height()+"px");
 	});
+
+
+function about()
+{	
+	if(homev==0)
+	{
+	$('#homediv').remove();
+	$('#audio').after(aboutd);	
+	homev=1;
+	$('body').css('overflow','auto');
+
+}
+};
+
+
+function home()
+{
+	if(homev==1)
+	{
+	$("#aboutdiv").remove();
+	$('#audio').after(homed);//homed is intialized in window ready because after 
+	}
+	homev=0;
+};
